676cc4e359790bcd6e6d2d3ac627a052c011db93
Glego <glego200323@gmail.com>
2025-07-01 01:18:45 +0300

    Solution fix FlyingRobot coords

--- app/main.py
+++ app/main.py
@@ -31,6 +31,8 @@ class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         if coords is None:
             coords = [0, 0, 0]
+        if len(coords) <= 2:
+            coords.append(0)
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
